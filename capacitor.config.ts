8e00040439b00c623685399fed68aa9d6b49e99c
Vicente Salinas Concha <166465673+VicenteSalinasConcha@users.noreply.github.com>
2026-09-03 17:21:23 -0400

    Cambio  de nombres en repositorios
    Se  cambio el nombre de JSON de proyecto.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cl.duoc.infoduoc',
+  appName: 'InfoDuoc',
+  webDir: 'dist'
+};
+
+export default config;
